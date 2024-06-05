e31b8205c8d97b39d03c245b8b04b1873de4bcba
oam <->
2024-06-05 09:29:54 +0200

    protobuf build script

--- protobuf.sh
+++ protobuf.sh
@@ -6,15 +6,21 @@ cd tf2_src/thirdparty
 # extracting package
 #
 gzip -d protobuf-2.6.1.tar.gz && tar -xf protobuf-2.6.1.tar
+
 rm protobuf-2.6.1.tar
-cd protobuf-2.6.1
+mv protobuf-2.6.1 protobuf-2.6.1-src
+mkdir protobuf-2.6.1
+
+cd protobuf-2.6.1-src
 
 # building package
 #
+echo "building protobuf-2.6.1 - please wait..."
+
 ./autogen.sh
 ./configure
 
-make
+make > /dev/null
 
 # build log
 #
@@ -22,4 +28,17 @@ touch ../../../protobuf.log
 date > ../../../protobuf.log
 
 make check | grep testsuite\ summary -A7 -i >> ../../../protobuf.log
+
+echo "finished..."
+
+# inform about results
+#
+failed1 = $(cat ../../../protobuf.log | grep xfail -i | awk '{print $2}')
+failed2 = $(cat ../../../protobuf.log | grep fail -i | awk '{print $2}')
+
+if [ "$failed1" -ne 0 ] || [ "$failed2" -ne 0 ]; then
+    echo "failed - see protobuf.log for details"
+else
+    echo "success"
+
 
