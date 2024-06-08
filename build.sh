00fe3e7f13666cd467b16a06c0bc6345ad132fe4
oam <->
2024-06-08 18:09:51 +0200

    small adjustments in build.sh

--- build.sh
+++ build.sh
@@ -8,14 +8,16 @@ git reset --hard
 ##################################################################################################
 #                                       BUILD PROTOBUF
 ##################################################################################################
-# entering thirdparty dir
-#
+
 tf2_src=$(pwd)/tf2_src
 
 build_tf2() 
 {
-    echo -e "\nBuild started..."
-    
+    echo -e "\nbuild started..."
+    echo -e "\nbuilding protobuf..." >> $tf2_src/../build.log
+
+    # entering thirdparty dir
+    #
     cd $tf2_src/thirdparty
 
     # extracting package
@@ -68,108 +70,128 @@ build_tf2()
 ##################################################################################################
 #                                       BUILD LIBRARIES
 ##################################################################################################
-# bitmap.a
-#
+
+    echo -e "\nbuilding libraries...\n" >> $tf2_src/../build.log
+
+    # bitmap.a
+    #
     cd $tf2_src/bitmap
     make -f bitmap_linux32.mak rebuild
     mv bitmap.a $tf2_src/lib/public/linux32
+    echo "done: bitmap.a" >> $tf2_src/../build.log
 
     # choreoobjects.a
     #
     cd $tf2_src/choreoobjects
     make -f choreoobjects_linux32.mak rebuild
     mv choreoobjects.a $tf2_src/lib/public/linux32
+    echo "done: choreoobjects.a" >> $tf2_src/../build.log
 
     # dmxloader.a
     #
     cd $tf2_src/dmxloader
     make -f dmxloader_linux32.mak rebuild
     mv dmxloader.a $tf2_src/lib/public/linux32
+    echo "done: dmxloader.a" >> $tf2_src/../build.log
 
     # mathlib.a
     #
     cd $tf2_src/mathlib
     make -f mathlib_linux32.mak rebuild
     mv mathlib.a $tf2_src/lib/public/linux32
+    echo "done: mathlib.a" >> $tf2_src/../build.log
 
     # particles.a
     #
     cd $tf2_src/particles
     make -f particles_linux32.mak rebuild
     mv particles.a $tf2_src/lib/public/linux32
+    echo "done: particles.a" >> $tf2_src/../build.log
 
     # replay_common.a
     #
     cd $tf2_src/replay/common
     make -f replay_common_linux32.mak rebuild
     mv replay_common.a $tf2_src/lib/common/linux32
+    echo "done: replay_common.a" >> $tf2_src/../build.log
 
     # replay.a
     #
     cd $tf2_src/replay
     make -f replay_linux32.mak rebuild
     mv replay.a $tf2_src/lib/public/linux32
+    echo "done: replay.a" >> $tf2_src/../build.log
 
     # tier1.a
     #
     cd $tf2_src/tier1
     make -f tier1_linux32.mak rebuild
     mv tier1.a $tf2_src/lib/public/linux32
+    echo "done: tier1.a" >> $tf2_src/../build.log
 
     # tier2.a
     #
     cd $tf2_src/tier2
     make -f tier2_linux32.mak rebuild
     mv tier2.a $tf2_src/lib/public/linux32
+    echo "done: tier2.a" >> $tf2_src/../build.log
 
     # tier3.a
     #
     cd $tf2_src/tier3
     make -f tier3_linux32.mak rebuild
     mv tier3.a $tf2_src/lib/public/linux32
+    echo "done: tier3.a" >> $tf2_src/../build.log
 
     # matsys.a
     #
     cd $tf2_src/vgui2/matsys_controls
     make -f matsys_controls_linux32.mak rebuild
     mv matsys_controls.a $tf2_src/lib/public/linux32
+    echo "done: matsys_controls.a" >> $tf2_src/../build.log
 
     # vgui_controls.a
     #
-    cd $tf2_src/vgui_controls
+    cd $tf2_src/vgui2/vgui_controls
     make -f vgui_controls_linux32.mak rebuild
     mv vgui_controls.a $tf2_src/lib/public/linux32
+    echo "done: vgui_controls.a" >> $tf2_src/../build.log
 
     # vtf.a
     #
     cd $tf2_src/vtf
     make -f vtf_linux32.mak rebuild
     mv vtf.a $tf2_src/lib/public/linux32
+    echo "done: vtf.a" >> $tf2_src/../build.log
 
     # gcsdk.a
     #
     cd $tf2_src/gcsdk
     make -f gcsdk_linux32.mak rebuild
     mv gcsdk.a $tf2_src/lib/public/linux32
+    echo "done: gcsdk.a" >> $tf2_src/../build.log
 
     # libtier0.so
     #
     cd $tf2_src/tier0
     make -f tier0_linux32.mak rebuild
     mv obj_tier0_linux32/release/libtier0.so $tf2_src/lib/public/linux32
+    echo "done: libtier0.so" >> $tf2_src/../build.log
 
     # libvstdlib.so
     #
     cd $tf2_src/vstdlib
     make -f vstdlib_linux32.mak rebuild
     mv obj_vstdlib_linux32/release/libvstdlib.so $tf2_src/lib/public/linux32
+    echo "done: libvstdlib.so" >> $tf2_src/../build.log
 
     # vpklib.a
     #
     cd $tf2_src/vpklib
     make -f vpklib_linux32.mak rebuild
     mv vpklib.a $tf2_src/lib/public/linux32
-
+    echo "done: vpklib.a" >> $tf2_src/../build.log
+    
 ##################################################################################################
 #                                       BUILD CLIENT
 ##################################################################################################
@@ -181,8 +203,8 @@ build_tf2()
 
     # build client
     # 
-    echo "building client.so..."
-    make -f client_linux32_tf.mak rebuild
+    echo -e "\n\nbuilding client.so...\n" >> $tf2_src/../build.log
+    make -f client_linux32_tf.mak rebuild >> $tf2_src/../build.log
 }
 
 ##################################################################################################
@@ -191,6 +213,7 @@ build_tf2()
 vpc_projgen()
 {
     echo -e "\Projgen initiated by user... Resetting all make files to defaults."    
+    echo -e "\n\nbuilding vpc projgen...\n" >> $tf2_src/../build.log
 
     # clear log for registering current build log
     #
@@ -208,31 +231,32 @@ vpc_projgen()
     echo "entering vpc directory..."
     cd tf2_src/devtools/bin
 
-    echo "launching vpc to generate projects..."
-    ./vpc_linux /v +launcher >> ../../../build.log
-    ./vpc_linux /v +launcher_main >> ../../../build.log
-    ./vpc_linux /v +engine >> ../../../build.log
-    ./vpc_linux /v +gamedlls /allgames >> ../../../build.log
+    echo -e "\n\nlaunching vpc to generate projects...\n" >> $tf2_src/../build.log
+
+    ./vpc_linux /v +launcher >> $tf2_src/../build.log
+    ./vpc_linux /v +launcher_main >> $tf2_src/../build.log
+    ./vpc_linux /v +engine >> $tf2_src/../build.log
+    ./vpc_linux /v +gamedlls /allgames >> $tf2_src/../build.log
 
     # generate additional dependency projects
     #
     echo "generating additional dependency projects..."
-    ./vpc_linux /v +bitmap >> ../../../build.log
-    ./vpc_linux /v +choreoobjects >> ../../../build.log
-    ./vpc_linux /v +dmxloader >> ../../../build.log
-    ./vpc_linux /v +mathlib >> ../../../build.log
-    ./vpc_linux /v +particles >> ../../../build.log
-    ./vpc_linux /v +replay_common >> ../../../build.log
-    ./vpc_linux /v +replay >> ../../../build.log
-    ./vpc_linux /v +tier1 >> ../../../build.log
-    ./vpc_linux /v +tier2 >> ../../../build.log
-    ./vpc_linux /v +tier3 >> ../../../build.log
-    ./vpc_linux /v +matsys_controls >> ../../../build.log
-    ./vpc_linux /v +vgui_controls >> ../../../build.log
-    ./vpc_linux /v +vtf >> ../../../build.log
-    ./vpc_linux /v +gcsdk >> ../../../build.log
-    ./vpc_linux /v +tier0 >> ../../../build.log
-    ./vpc_linux /v +vstdlib >> ../../../build.log
+    ./vpc_linux /v +bitmap >> $tf2_src/../build.log
+    ./vpc_linux /v +choreoobjects >> $tf2_src/../build.log
+    ./vpc_linux /v +dmxloader >> $tf2_src/../build.log
+    ./vpc_linux /v +mathlib >> $tf2_src/../build.log
+    ./vpc_linux /v +particles >> $tf2_src/../build.log
+    ./vpc_linux /v +replay_common >> $tf2_src/../build.log
+    ./vpc_linux /v +replay >> $tf2_src/../build.log
+    ./vpc_linux /v +tier1 >> $tf2_src/../build.log
+    ./vpc_linux /v +tier2 >> $tf2_src/../build.log
+    ./vpc_linux /v +tier3 >> $tf2_src/../build.log
+    ./vpc_linux /v +matsys_controls >> $tf2_src/../build.log
+    ./vpc_linux /v +vgui_controls >> $tf2_src/../build.log
+    ./vpc_linux /v +vtf >> $tf2_src/../build.log
+    ./vpc_linux /v +gcsdk >> $tf2_src/../build.log
+    ./vpc_linux /v +tier0 >> $tf2_src/../build.log
+    ./vpc_linux /v +vstdlib >> $tf2_src/../build.log
 
     echo -e "\nfinished generating projects..."
     echo "leaving vpc directory..."
